296131dffb05967fdd48950403085d64ef3e31e0
Ailin Huang <ailin.h58@gmail.com>
2020-07-15 18:39:45 +0200

    friendship gem fully implemented - still need to work on AJAX

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :pages do
+    collection do
+      post 'add_friend/:id', to: 'pages#add_friend', as: :add_friend
+      post 'accept_friend/:id', to: 'pages#accept_friend', as: :accept_friend
+      delete 'decline_friend/:id', to: 'pages#decline_friend', as: :decline_friend
+    end
+  end
+
 
   resources :homes
+
 end
